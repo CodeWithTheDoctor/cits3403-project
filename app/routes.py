973133e06c3bb252670faf2c74875728fac30059
tim <timothy.tan.93@gmail.com>
2022-03-08 09:47:56 +0800

    adding form

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World"
+    user = {"username": "Miguel"}
+    posts = [
+        {"author": {"username": "john"}, "body": "Beautiful day in portland!"},
+        {"author": {"username": "susan"}, "body": "The avengers movie was so cool!"},
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
