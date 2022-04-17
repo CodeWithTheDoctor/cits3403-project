a175a9b4d4cadb1177a331ffd69c0932ce339e13
tim <timothy.tan.93@gmail.com>
2022-04-17 17:40:49 +0800

    formatting

--- config.py
+++ config.py
@@ -2,17 +2,11 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# TODO: check what the secret_key does
+
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
 
-    # development
-    # SQLALCHEMY_DATABASE_URI = os.environ.get(
-    #     "DATABASE_URL"
-    # ) or "sqlite:///" + os.path.join(basedir, "app.db")
-    #
-
-    # heroku testing
+    # for deployment on heroku
     # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "").replace(
     #     "postgres://", "postgresql://"
     # ) or "sqlite:///" + os.path.join(basedir, "app.db")
