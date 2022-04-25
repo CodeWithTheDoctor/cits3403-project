37efb48cee56b9133e587da28034bcc048dcdb08
tim <timothy.tan.93@gmail.com>
2022-04-25 16:30:14 +0930

    fix imports

--- akari3403.py
+++ akari3403.py
@@ -1,7 +1,9 @@
+from distutils.command.config import config
 from app import app, db
 from app.models import User, Post, Puzzle
+from config import Config
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "User": User, "Post": Post, "Puzzle": Puzzle}
+    return {"db": db, "User": User, "Post": Post, "Puzzle": Puzzle, "config": Config}
