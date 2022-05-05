da65414d31e2f9858163919f49ba27788e529220
tim <timothy.tan.93@gmail.com>
2022-05-05 17:52:46 +1000

    association object is working

--- akari3403.py
+++ akari3403.py
@@ -1,6 +1,6 @@
 from distutils.command.config import config
 from app import app, db
-from app.models import User
+from app.models import User, Puzzle, User_Puzzle
 from config import Config
 
 
@@ -10,4 +10,6 @@ def make_shell_context():
         "db": db,
         "User": User,
         "config": Config,
+        "Puzzle": Puzzle,
+        "User_Puzzle": User_Puzzle,
     }
--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     score = db.Column(db.Integer, index=True)
-    puzzles = db.Column(db.Integer)
+    puzzles = db.relationship("User_Puzzle", back_populates="user")
 
     def set_password(self, password) -> None:
         self.password_hash = generate_password_hash(password)
@@ -25,6 +25,20 @@ class User(UserMixin, db.Model):
 
     def __repr__(self) -> str:
         return "<User {}>".format(self.username)
+
+
+class Puzzle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    config = db.Column(db.String(150), unique=True)
+    users = db.relationship("User_Puzzle", back_populates="puzzle")
+
+
+class User_Puzzle(db.Model):
+    user_id = db.Column(db.ForeignKey(User.id), primary_key=True)
+    puzzle_id = db.Column(db.ForeignKey(Puzzle.id), primary_key=True)
+    time = db.Column(db.Float)
+    puzzle = db.relationship("Puzzle", back_populates="users")
+    user = db.relationship("User", back_populates="puzzles")
 
 
 # Posts is for reference only, not used in production
