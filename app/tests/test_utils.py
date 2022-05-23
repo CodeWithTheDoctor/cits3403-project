13c7e9490b8e18aa5a1448c727b47ac4cbc196ed
tim <timothy.tan.93@gmail.com>
2022-05-23 10:28:10 +0800

    add utils test

---
+++ app/tests/test_utils.py
@@ -0,0 +1,12 @@
+import pytest
+from app.api.utils import check_puzzle, validate_puzzle
+
+
+@pytest.mark.skip(reason="not yet implemented")
+def test_check_puzzle():
+    assert check_puzzle()
+
+
+@pytest.mark.skip(reason="not yet implemented")
+def test_validate_puzzle():
+    assert validate_puzzle()
