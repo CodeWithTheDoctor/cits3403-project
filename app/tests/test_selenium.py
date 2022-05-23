29558568e9e26bc31821f990b7434a9c4518e9bf
tim <timothy.tan.93@gmail.com>
2022-05-23 10:05:50 +0800

    more selenium tests

--- app/tests/test_selenium.py
+++ app/tests/test_selenium.py
@@ -10,30 +10,39 @@ from app.models import User
 from app import db
 
 
-def test_selenium():
+def test_selenium(app_ctx):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("http://127.0.0.1:5000/auth/register")
     driver.maximize_window()
 
-    # Test register user
+    # Test register user with existing user
     driver.find_element(By.ID, "username").send_keys("test_user")
     driver.find_element(By.ID, "password").send_keys("password")
     driver.find_element(By.ID, "password2").send_keys("password")
     driver.find_element(By.ID, "email").send_keys("test@example.com")
     driver.find_element(By.ID, "submit").click()
 
-    sleep(1)
-    # assert "test" in driver
+    user_span = driver.find_elements(By.TAG_NAME, "span")[1].text
+
+    assert user_span == "[Please use a different username]"
 
     sleep(1)
+
+    # Test user can login
     driver.get("http://127.0.0.1:5000/auth/login")
     driver.find_element(By.ID, "username").send_keys("david")
     driver.find_element(By.ID, "password").send_keys("password")
     driver.find_element(By.ID, "submit").click()
 
-    test_user = User.query.filter_by(username="test_user").first()
-    db.session.delete(test_user)
-    db.session.commit()
+    result = driver.find_element(By.ID, "startButton").text
+
+    assert result == "Start"
+
+    driver.get("http://127.0.0.1:5000/auth/logout")
+
+    login = driver.find_elements(By.TAG_NAME, "h1")
+
+    assert login[1].text == "Sign In"
 
     sleep(1)
     driver.close()
